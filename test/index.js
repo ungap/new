745813e3c03806683bcb93bee9c188016f7310cb
webreflection <andrea.giammarchi@gmail.com>
2023-08-07 11:31:00 +0200

    added more tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,14 @@
 require('../index.js');
 
 console.assert(Date.new(0) instanceof Date);
+
+let constructTrap = false;
+let result = (new Proxy(Date, {
+  construct(target, args) {
+    constructTrap = true;
+    return new target(...args);
+  }
+})).new();
+
+console.assert(constructTrap);
+console.assert(result instanceof Date);
